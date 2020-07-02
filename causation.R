27ef48f9e5f9f73881dd30250c5e001e51292286
andreashih <r08142004@ntu.edu.tw>
2020-07-02 16:36:44 +0800

    0702

--- causation.R
+++ causation.R
@@ -2,11 +2,23 @@ library(readr)
 library(stringr)
 library(dplyr)
 
-concord_shi <- read_csv("concord_shi.csv", 
+concord_shi <- read_csv("causation_sketch_engine_shi.csv")
+
+concord_rang <- read_csv("causation_sketch_engine_rang.csv", 
                         col_names = FALSE, skip = 4)
 
-concord_rang <- read_csv("concord_rang.csv", 
-                        col_names = FALSE, skip = 4)
+concord_shi2 <- concord_shi %>%
+  mutate(pre = sapply(str_split(pre, "</s><s>"), tail, 1), 
+         post = sapply(str_split(post, "</s><s>"), head, 1))
+
+concord_rang2 <- concord_rang %>%
+  mutate(X2 = sapply(str_split(X2, "</s><s>"), tail, 1), 
+         X4 = sapply(str_split(X4, "</s><s>"), head, 1))
+
+write_csv(concord_shi2, "C:\\Users\\user\\Desktop\\practice_in_2020\\concord_shi.csv")
+
+write_csv(concord_rang2, "C:\\Users\\user\\Desktop\\practice_in_2020\\concord_rang.csv")
+
 
 concord_rang %>%
   filter(str_detect(X2, "^[，。][^(，|。)]+/N[^(，|。)]+$")) -> a
@@ -26,4 +38,6 @@ get_actor <- function(left_context, actor_pat) {
       break
   }
 }
+
+get_actor(concord_rang$X2, "(?<=(/NN|/NR))[^(/N)]+(?=(/NN|/NR))")
 
